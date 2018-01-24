34aaa02a0857c19353aed8464b85774fe66d3667
Erica Windisch <erica@iopipe.com>
2018-01-24 02:43:50 +0000

    Gofmt
    Signed-off-by: Erica Windisch <erica@iopipe.com>

--- c.go
+++ c.go
@@ -1,4 +1,5 @@
 package main
+
 // typedef void (*voidFunc) (char *context, char *event);
 // void callCfuncPointer(voidFunc ptr, char *context, char *event) {
 //    return ptr(context, event);
--- lambda.go
+++ lambda.go
@@ -1,27 +1,28 @@
 package main
+
 // typedef void (*voidFunc) (char *context, char *event);
 // void callCfuncPointer(voidFunc ptr, char *context, char *event);
 import "C"
 
 import (
-  "fmt"
-  "log"
-  "os"
-  "net/rpc"
-  "net"
-  "unsafe"
-  "context"
-  "time"
-  "github.com/aws/aws-lambda-go/lambda/messages"
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda/messages"
+	"log"
+	"net"
+	"net/rpc"
+	"os"
+	"time"
+	"unsafe"
 )
 
 func makeHandler(cFunction unsafe.Pointer) func(ctx, payload []byte) (result []byte, err error) {
-  return func(ctx, payload []byte) (result []byte, err error) {
-    C.callCfuncPointer((*[0]byte)(cFunction), C.CString(string(ctx[:])), C.CString(string(payload[:])));
-    result = []byte("Here is a result....")
-    err = nil
-    return
-  }
+	return func(ctx, payload []byte) (result []byte, err error) {
+		C.callCfuncPointer((*[0]byte)(cFunction), C.CString(string(ctx[:])), C.CString(string(payload[:])))
+		result = []byte("Here is a result....")
+		err = nil
+		return
+	}
 }
 
 type Function struct {
@@ -31,15 +32,15 @@ type Function struct {
 
 func (fn *Function) Invoke(req *messages.InvokeRequest, response *messages.InvokeResponse) error {
 	/*defer func() {
-		if err := recover(); err != nil {
-			panicInfo := getPanicInfo(err)
-			response.Error = &messages.InvokeResponse_Error{
-				Message:    panicInfo.Message,
-				Type:       getErrorType(err),
-				StackTrace: panicInfo.StackTrace,
-				ShouldExit: true,
-			}
-		}
+	  if err := recover(); err != nil {
+	    panicInfo := getPanicInfo(err)
+	    response.Error = &messages.InvokeResponse_Error{
+	      Message:    panicInfo.Message,
+	      Type:       getErrorType(err),
+	      StackTrace: panicInfo.StackTrace,
+	      ShouldExit: true,
+	    }
+	  }
 	}()*/
 
 	deadline := time.Unix(req.Deadline.Seconds, req.Deadline.Nanos).UTC()
@@ -49,10 +50,10 @@ func (fn *Function) Invoke(req *messages.InvokeRequest, response *messages.Invok
 	payload, err := fn.handler(req.ClientContext, req.Payload)
 	if err != nil {
 		response.Error = &messages.InvokeResponse_Error{
-      Message: err.Error(),
-      Type:    "GoError",
-    }
-    return nil
+			Message: err.Error(),
+			Type:    "GoError",
+		}
+		return nil
 	}
 	response.Payload = payload
 	return nil
@@ -60,18 +61,18 @@ func (fn *Function) Invoke(req *messages.InvokeRequest, response *messages.Invok
 
 //export Start
 func Start(userHandler unsafe.Pointer) {
-  port := os.Getenv("_LAMBDA_SERVER_PORT")
-  lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", port))
-  if err != nil {
-    log.Fatal(err)
-  }
+	port := os.Getenv("_LAMBDA_SERVER_PORT")
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", port))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  function := new(Function)
-  function.handler = makeHandler(userHandler)
+	function := new(Function)
+	function.handler = makeHandler(userHandler)
 
-  rpc.Register(function)
-  rpc.Accept(lis)
-  log.Fatal("accept should not have returned")
+	rpc.Register(function)
+	rpc.Accept(lis)
+	log.Fatal("accept should not have returned")
 }
 
-func main() { }
+func main() {}
